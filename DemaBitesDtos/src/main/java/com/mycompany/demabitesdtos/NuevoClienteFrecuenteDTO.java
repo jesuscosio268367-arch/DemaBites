1883261cb7d85619ee922c2ef44b502680e38090
Dario <Dario@DESKTOP-ICUR4FM>
2026-03-25 16:29:08 -0700

    Commit DTOs Cliente

--- DemaBitesDtos/src/main/java/com/mycompany/demabitesdtos/NuevoClienteFrecuenteDTO.java
+++ DemaBitesDtos/src/main/java/com/mycompany/demabitesdtos/NuevoClienteFrecuenteDTO.java
@@ -5,31 +5,37 @@
 package com.mycompany.demabitesdtos;
 
 /**
- * DTO para el registro de un nuevo cliente.
- * @author Jesus Omar
+ * DTO para el registro de un nuevo cliente frecuente.
+ * @author Dario
  */
-public class NuevoClienteDTO {
+public class NuevoClienteFrecuenteDTO {
     private String nombres;
     private String apellidoPaterno;
     private String apellidoMaterno;
+    private String telefono;
+    private String email;
 
     /**
      * Contructor vacio.
      */
-    public NuevoClienteDTO() {
+    public NuevoClienteFrecuenteDTO() {
         
     }
-
+     
     /**
      * Constructor con todos los campos.
      * @param nombres Los nombres del cliente.
      * @param apellidoPaterno El apellido paterno.
      * @param apellidoMaterno El apellido materno.
+     * @param telefono El telefono del cliente.
+     * @param email El email del cliente.
      */
-    public NuevoClienteDTO(String nombres, String apellidoPaterno, String apellidoMaterno) {
+    public NuevoClienteFrecuenteDTO(String nombres, String apellidoPaterno, String apellidoMaterno, String telefono, String email) {
         this.nombres = nombres;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
+        this.telefono = telefono;
+        this.email = email;
     }
 
     /** @return El nombre o nombres del cliente. */
@@ -45,6 +51,16 @@ public class NuevoClienteDTO {
     /** @return El apellido materno del cliente. */
     public String getApellidoMaterno() {
         return apellidoMaterno;
+    }
+
+    /** @return El telefono del cliente. */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /** @return El email del cliente. */
+    public String getEmail() {
+        return email;
     }
     
 }
