6a0a4df1c67a9e900124e35dbab39c693e1533b2
jesuscosio268367-arch <jesus.cosio268367@potros.itson.edu.mx>
2026-03-25 17:38:35 -0600

    ClientesDominio

---
+++ DemaBitesDominio/src/main/java/com/mycompany/demabitesdominio/Cliente.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demabitesdominio;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jesus Omar
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_cliente")
+    private Long id;
+    
+    @Column(name = "nombre", nullable = false)
+    private String nombres;
+    
+    @Column(name = "apellidoPaterno", nullable = false)
+    private String apellidoPaterno;
+    
+    @Column(name = "apellidoMaterno", nullable = false)
+    private String apellidoMaterno;
+
+    public Cliente(){
+    }
+
+    public Cliente(String nombres, String apellidoPaterno, String apellidoMaterno) {
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.demabitesdominio.Cliente[ id=" + id + " ]";
+    }
+    
+}
---
+++ DemaBitesDominio/src/main/java/com/mycompany/demabitesdominio/ClienteFrecuente.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demabitesdominio;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jesus Omar
+ */
+@Entity
+@Table(name = "clientesFrecuentes")
+public class ClienteFrecuente extends Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Column(name = "telefono", nullable = false)
+    private String telefono;
+    
+    @Column(name = "email", nullable = true)
+    private String email;
+    
+    @Column(name = "fechaRegistro", nullable = false)
+    private LocalDate fechaRegistro;
+
+    public ClienteFrecuente(){
+    }
+
+    public ClienteFrecuente(String telefono, String email, LocalDate fechaRegistro, String nombres, String apellidoPaterno, String apellidoMaterno) {
+        super(nombres, apellidoPaterno, apellidoMaterno);
+        this.telefono = telefono;
+        this.email = email;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+    
+}
